af4c3c75d5ffa204ee9534df260f5e691675fa20
Jaroca124 <jacob.caccamo@tufts.edu>
2017-02-02 11:05:14 -0500

    adding movement and environment updates

--- CAA_Virtual/Assets/movement.cs
+++ CAA_Virtual/Assets/movement.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
 
-    float speed = 5.0f;
+    //float speed = 10.0f;
+    //public float thrust;
+    //public Rigidbody drone;
 
-    void Start()
+    public float speed = 90f;
+    public float rollFactor = 5f;
+    private float powerInput;
+    private Rigidbody drone;
+
+    void Awake()
     {
-
+        drone = GetComponent<Rigidbody>();
     }
-
+     
     void Update()
     {
-        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * speed * Time.deltaTime;
+        powerInput = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        drone.AddRelativeForce(powerInput * speed, 0f, 0f);
+        //float z = powerInput * 15.0f; // might be negative, just test it
+        //Vector3 euler = transform.localEulerAngles;
+        //euler.z = Mathf.Lerp(euler.z, z, 2.0f * Time.deltaTime);
+        //transform.localEulerAngles = euler;
     }
 }
