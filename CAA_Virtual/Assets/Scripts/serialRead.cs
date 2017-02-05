fb0a74a491ed83df83eb824085a9c4b7bf128862
Jaroca124 <jacob.caccamo@tufts.edu>
2017-02-05 16:11:00 -0500

    Adding Serial Read Script
    Having trouble resolving the serial communication errors with arduino

---
+++ CAA_Virtual/Assets/Scripts/serialRead.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+public class serialRead : MonoBehaviour {
+
+    // Access Arduino COM Port
+    SerialPort stream = new SerialPort("COM3", 9600); //Set the port (com4) and the baud rate (9600, is standard on most devices)
+
+    // Use this for initialization
+    void Start () {
+        stream.Open(); //Open the Serial Stream.
+        stream.ReadTimeout = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (stream.IsOpen)
+        {
+            try
+            {
+                SetAutonomyMode(stream.ReadByte());
+                print(stream.ReadByte());
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    void SetAutonomyMode(int input)
+    {
+        print("Success");
+    }
+}
