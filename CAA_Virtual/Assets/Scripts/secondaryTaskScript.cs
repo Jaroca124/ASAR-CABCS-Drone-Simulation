e6bbad7bfca843fbe10b5a3d3c444f2157f877c0
Jaroca124 <jacob.caccamo@tufts.edu>
2017-02-17 10:24:59 -0500

    Multiple Displays Work

---
+++ CAA_Virtual/Assets/Scripts/secondaryTaskScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondaryTaskScript : MonoBehaviour {
+
+    string[] q_text_array = ["What is your current fuel efficiency?",
+                "What is your current elevation?",
+                "What is your current elevation minus 17?",
+                "What is your current fuel efficiency plus 56?",
+                "Report the sum of the X coordinates of all active drones.",
+                "Report the sum of the Y coordinates of all active drones."];
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
