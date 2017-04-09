b3f00d909bccc033ed4dd59eea51fcda5872cb5b
Jaroca124 <jacob.caccamo@tufts.edu>
2017-04-08 20:52:13 -0400

    Log Data Works

---
+++ CAA_Virtual/Assets/Scripts/Primary Task/changeTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeTarget : MonoBehaviour {
+
+    public Sprite[] orderA; //= new Sprite[6] { block3, block1, block2, block4, block5, finish };
+    public Sprite[] orderB; //= new Sprite[6] { block5, block3, block4, block1, block2, finish };
+
+    Image target;
+
+    int targetNum = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = this.GetComponent<Image>();
+        target.sprite = orderA[targetNum];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Use Trial Number to Decide Order
+        if (Input.GetButtonDown("BButton"))
+        {
+            if (targetNum < 6)
+            {
+                target.sprite = orderA[targetNum];
+                targetNum++;
+            }
+        }
+    }
+}
--- CAA_Virtual/Assets/Scripts/logData.cs
+++ CAA_Virtual/Assets/Scripts/logData.cs
@@ -1,15 +1,41 @@
 ﻿using System.IO;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class logData : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        //string[] lines = { "First line", "Second line", "Third line" };
-        // WriteAllLines creates a file, writes a collection of strings to the file,
-        // and then closes the file.  You do NOT need to call Flush() or Close().
-        //System.IO.File.WriteAllLines(@"C:\Users\Jake\Documents\test", lines);
+    public InputField expName;
+    public InputField partNumber;
+    public InputField partAge;
+    public InputField partGender;
+
+    void Start()
+    {
+        
+    }
+
+    public void Savecsv() {
+
+        string Name = expName.text;
+        string Number = partNumber.text;
+        string Age = partAge.text;
+        string Gender = partGender.text;
+
+        string filePath = "C:/Users/Jake/Documents/GitHub/CAA_Virtual/CAA_Virtual/Assets/Data/data.csv";
+        string delimiter = ",";
+
+        string[][] output = new string[][]{
+             new string[]{"Experiment Name", "Participant Number", "Participant Age", "Participant Gender"},
+             new string[]{Name, Number, Age, Gender}
+         };
+        int length = output.GetLength(0);
+        StringBuilder sb = new StringBuilder();
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+
+        File.WriteAllText(filePath, sb.ToString());
     }
 }
